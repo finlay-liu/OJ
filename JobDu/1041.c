bd44feb214bb6c6ee60b7dabb4b66ffd8b03bd86
finlay-liu <finlaylyz@gmail.com>
2015-03-04 14:00:37 +0800

    Add 1041

---
+++ JobDu/1041.c
@@ -0,0 +1,16 @@
+// http://ac.jobdu.com/problem.php?pid=1041
+#include <stdio.h>
+int main()
+{
+	int n, i;
+	int num[100];
+	while(scanf("%d",&n) != EOF)
+	{
+		memset(num , -1, sizeof(int));
+		for(i = 0;i < n;i++)
+		{
+			
+		}
+	}
+	return 0;
+}
